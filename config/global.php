c0c7c6eb36a66fb2f641f0451b7e35b4adecc56b
ElvisDavis <davis-elvis@hotmail.com>
2023-10-31 22:31:48 -0500

    conexionBD

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+//IP de la pc servidor de la base de dato
+define("DB_HOST","localhost");
+//Nombre de la base de datos
+define("DB_NAME","sri");
+
+//usuario de la base de datos
+
+define("DB_USERNAME","root");
+
+//contraseña de la base de datos
+define("DB_PASSWORD","");
+
+//definimos la codificación de los caracteres
+define("DB_ENCODE","utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE","Comprobante");
+?>
