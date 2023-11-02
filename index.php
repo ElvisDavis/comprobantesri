f2ade6db4c5a6ca7d65ad8df39a6450a64a5d695
ElvisDavis <davis-elvis@hotmail.com>
2023-11-02 08:52:42 -0500

    index trabajando

--- index.php
+++ index.php
@@ -1,4 +1,71 @@
-<?php
-//redireccionamos a la vista del login
-header ('Location: vistas/login.html');
-?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ECUA FACTURAS|DESCARGA COMPROBANTES</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">Ecua Facturas<span class="text-primary">.</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item d-flex">
+                        <a class="nav-link align-self-center text-dark" >Personas</a>
+                    </li>
+                    <li class="nav-item d-flex">
+                        <a class="nav-link align-self-center text-dark" href="#">Empresas</a>
+                    </li>
+                    <li class="nav-item d-flex">
+                        <a class="nav-link align-self-center text-dark">Nosotros</a>
+                    </li>
+                    <li class="nav-item me-2 d-flex">
+                        <a class="nav-link align-self-center text-dark">Inicia Sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-primary">Descargar la app</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <section id="hero">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="content mx-auto">
+                   <small class="d-block text-primary mb-3"> <span class="badge bg-primary me-2">Nuevo</span> Adquiere nuestros servicios</small> 
+                   <h1 class="display-4 fw-bold mb-4">Tu mejor aliado con tus impuestos<span class="text-primary">.</span></h1>
+                   <p class="lead-mb-4">Dile adiós al SRI y obten tus comprobantes de una manera mas rápida y efectiva
+                    100% digital.
+                   </p>
+                   <a href="#" class="btn btn-primary">Abre tu cuenta hoy</a>
+                   <div>
+                    <div></div>
+                    <div></div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
