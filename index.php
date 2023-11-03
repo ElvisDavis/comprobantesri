204797ed975b0e82f2c347ae71bde3ba575b9ddb
ElvisDavis <davis-elvis@hotmail.com>
2023-11-02 20:16:04 -0500

    segunda seccion index

--- index.php
+++ index.php
@@ -9,23 +9,24 @@
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="public/css/font-awesome.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-transparent">
+    <nav class="navbar navbar-expand-lg bg-transparent position-absolute w-100">
         <div class="container">
             <a class="navbar-brand fw-bold" href="#">Ecua Facturas<span class="text-primary">.</span></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <i class="fa fa-bars lead text-dark"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item d-flex">
-                        <a class="nav-link align-self-center text-dark" >Personas</a>
+                        <a class="nav-link align-self-center text-dark">Personas</a>
                     </li>
                     <li class="nav-item d-flex">
                         <a class="nav-link align-self-center text-dark" href="#">Empresas</a>
@@ -37,7 +38,7 @@
                         <a class="nav-link align-self-center text-dark">Inicia Sesión</a>
                     </li>
                     <li class="nav-item">
-                        <a class="btn btn-primary">Descargar la app</a>
+                        <a class="btn btn-primary">Abre tu cuenta aquí.</a>
                     </li>
                 </ul>
 
@@ -46,19 +47,57 @@
     </nav>
 
     <section id="hero">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="content mx-auto">
-                   <small class="d-block text-primary mb-3"> <span class="badge bg-primary me-2">Nuevo</span> Adquiere nuestros servicios</small> 
-                   <h1 class="display-4 fw-bold mb-4">Tu mejor aliado con tus impuestos<span class="text-primary">.</span></h1>
-                   <p class="lead-mb-4">Dile adiós al SRI y obten tus comprobantes de una manera mas rápida y efectiva
-                    100% digital.
-                   </p>
-                   <a href="#" class="btn btn-primary">Abre tu cuenta hoy</a>
-                   <div>
-                    <div></div>
-                    <div></div>
-                   </div>
+        <div class="row g-0 h-100">
+            <div class="col-lg-6 d-flex">
+                <div class="content mx-auto align-self-center px-4 my-5">
+                    <small class="d-block text-primary mb-3"> <span class="badge bg-primary me-2">Nuevo</span> Adquiere
+                        nuestros servicios</small>
+                    <h1 class="display-4 fw-bold mb-4">Tu mejor aliado con tus impuestos<span
+                            class="text-primary">.</span></h1>
+                    <p class="lead-mb-4">Dile adiós al SRI y obten tus comprobantes de una manera mas rápida y efectiva
+                        100% digital.
+                    </p>
+                    <a href="#" class="btn btn-primary mb-5">Abre tu cuenta hoy</a>
+                    <div class="d-flex me-4">
+                        <div class="d-flex">
+                            <img src="img/what.png" class="me-3">
+                            <div>
+                                <small>Contactate</small>
+                                <p class="fw-bold mb-0">Con nosotros</p>
+                            </div>
+                        </div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 d-flex bg-light">
+                <div class="content mx-auto align-self-center px-4 my-5">
+                    <img src="img/impuesto.jpg" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--secccion 2-->
+    <section class="d-flex">
+        <div class="comtainer">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="content">
+                        <img src="img/tax.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <h1 class="display-4 fw-bold">Haz que tu tiempo rinda<span class="text-primary">.</span></h1>
+                    <ul class="list-unstyled">
+                        <li class="d-flex mb-4">
+                            <p class="text-primary lead fw-bold step">01</p>
+                            <div class="ms-3">
+                                <p class="lead fw-bold">Crea tu cuenta<span class="text-primary">.</span></p><p>Es muy, sencillo solamente
+                                    ingresa tus datos.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
